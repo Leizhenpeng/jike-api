f2dc977d53b0788086db0fc4db144bb1f4bcc848
river <laolei@forkway.cn>
2024-05-15 20:30:14 +0800

    Update API configuration in tests

--- tests/config.example.ts
+++ tests/config.example.ts
@@ -7,13 +7,13 @@ import { resolveApiConfig } from '../src'
 // Run `jike-cli user info -rP`
 
 export const config = resolveApiConfig({
-  endpointId: 'keji',
-  endpointUrl: 'https://keji.org/api/',
-  bundleId: 'org.keji.keji',
-  appVersion: '1.2.3',
-  buildNo: '10086',
-  userAgent: 'keji-useragent',
-
+  endpointId: 'jike',
+  endpointUrl: 'https://api.ruguoapp.com/',
+  bundleId: 'com.ruguoapp.jike',
+  buildNo: '2961',
+  appVersion: '7.56.2',
+  userAgent:
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Jike/7.56.2 /sa-sdk-ios/sensors-verify/tr.ruguoapp.com?jike',
   deviceId: '766ECA1A-C654-11EC-89F9-4F2C5FBE0E82',
   idfv: '82283B5C-C654-11EC-B221-BBD1DABE7B57',
   accessToken: 'paste-your-access-token',
